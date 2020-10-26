9ca6e3a1e2dd890e33ea31db0f4155506cb92654
czy71988 <czy71988@163.com>
2020-10-26 10:18:07 +0800

    分页问题

--- src/components/shop/shopType.vue
+++ src/components/shop/shopType.vue
@@ -64,7 +64,7 @@
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
           :current-page.sync="currentPage1"
-          :page-size="100"
+          :page-size="10"
           layout="total, prev, pager, next"
           :total="total">
         </el-pagination>
